14f752c99f018e16017489c2244ad78418c20d7e
nganhkhoa <service.nganhkhoa@gmail.com>
2025-01-24 16:10:43 -0600

    [dev] warn before overwrite old report

--- runner/reporter.py
+++ runner/reporter.py
@@ -9,11 +9,20 @@ class Reporter:
     self.name = name
     filename = f"{name}_{tag}.tar.gz"
     filepath = self.storage / filename
+
+    if filepath.exists():
+      print("Found existing report file", filepath)
+      print("Please manually keep the file or move")
+      print("append to tar.gz is unsupported by Python")
+      print("Ctrl+C to exit")
+      print("Press any key to continue, overwrite")
+      input()
     self.tar = tarfile.open(filepath, 'w:gz')
 
   def add_report(self, commit, file):
     name = f"{self.name}_{commit}_{self.tag}.json"
     self.tar.add(file, arcname=name)
+    print("add report", name)
 
   def finalize_report(self):
     self.tar.close()
