7ad6d6b3601759ea4eb6255402958688cd55e893
nganhkhoa <service.nganhkhoa@gmail.com>
2025-01-30 23:41:03 -0600

    [dev] infer with new architecture

--- runner/infer.py
+++ runner/infer.py
@@ -9,7 +9,7 @@ class Infer(Tool):
 
   def run(self, project):
     # we only need the project built for infer
-    project.build()
+    project.build(self)
 
     report = self.cwd / "infer-out" / "report.sarif"
     if not report.exists():
@@ -17,9 +17,9 @@ class Infer(Tool):
     return report
 
   def invoke(self, command, **kargs):
-    kargs['cmake']
-    kargs['make']
-    kargs['script']
+    cmake = kargs.get("cmake", False)
+    make = kargs.get("make", False)
+    script = kargs.get("script", False)
 
     base = ['infer']
 
@@ -32,10 +32,10 @@ class Infer(Tool):
       base += ['run', '--sarif', '--bufferoverrun', '--pulse']
       # do a clean first
       subprocess.run([*command, 'clean'], cwd=self.cwd)
-      subprocess.run(command, cwd=self.cwd)
+      subprocess.run([*base, '--', *command], cwd=self.cwd, check=True)
     if cmake:
       # we delete the build folder first
       # this would delete lz4 build folder
       shutil.rmtree(self.cwd / "_build", ignore_errors=True)
       base += ['compile']
-      subprocess.run([*base, '--', *command], cwd=self.cwd)
+      subprocess.run([*base, '--', *command], cwd=self.cwd, check=True)
